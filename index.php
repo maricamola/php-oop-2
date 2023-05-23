e9bb85e877b12773e98f13c5426a4692a1765c06
maricamola <maricamola98@libero.it>
2023-05-23 17:01:43 +0200

    cards created

--- index.php
+++ index.php
@@ -2,41 +2,34 @@
 require_once 'models/Prodotto.php';
 
 $prodotti = [
-  new Prodotto('models/immagini/cuccia.jpg', 'Cuccia per Cani', '29.99€', 'Cani'),
-  new Prodotto('models/immagini/cibo.jpg', 'Cibo per Cani', '10.99€', 'Cani'),
-  new Prodotto('models/immagini/gioco.jpg', 'Gioco per Gatti', '5.99€', 'Gatti'),
+  new Prodotto('models/immagini/cibo.jpg', 'Cibo gusto pollo', 'Cani', '69.99€'),
+  new Prodotto('models/immagini/cibo3.jpg', 'Cibo gusto coniglio', 'Cani', '21.99€'),
+  new Prodotto('models/immagini/cibo2.jpg', 'Cibo gusto pollo', 'Cani', '56.99€'),
+  new Prodotto('models/immagini/cuccia3.jpg', 'Cuccia in legno da esterno', 'Cani', '29.99€'),
+  new Prodotto('models/immagini/gioco.jpg', 'Gioco in legno per pareti', 'Gatti', '5.99€'),
+  new Prodotto('models/immagini/cuccia2.jpg', 'Cuccia in legno da esterno', 'Cani', '29.99€'),
 ];
+
+include __DIR__ . '/layout/header.php';
+include __DIR__ . '/layout/head.php';
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <title>Animalstore | Shop-Online</title>
-</head>
-
 <body>
-
-  <header>
-    <div class="container p-2 text-center">
-      <h2>Animalstore</h2>
-    </div>
-  </header>
-  <div class="container my-5">
+  <div class="container my-5 d-flex flex-wrap justify-content-center">
     <?php foreach ($prodotti as $prodotto) : ?>
-      <div class="card" style="width: 18rem;">
-        <img src="<?php $prodotto->getImmagine(); ?>" class="card-img-top" alt="image">
-        <div class="card-body">
-          <h5 class="card-title"><?php $prodotto->getNome(); ?></h5>
-          <p class="card-text"><?php $prodotto->getPrezzo(); ?></p>
-          <a href="#" class="btn btn-primary"><?php $prodotto->getMarca(); ?></a>
-        </div>
+    <div class="card m-4" style="width: 18rem;">
+      <img src="<?php $prodotto->getImmagine(); ?>" class="card-img-top" alt="image">
+      <div class="card-body">
+        <h5 class="card-title"><?php $prodotto->getNome(); ?></h5>
+        <p class="card-text"><?php $prodotto->getPrezzo(); ?></p>
+        <p class="card-text"><?php $prodotto->getMarca(); ?></p>
+        <a href="#" class="btn btn-primary">Acquista</a>
       </div>
+    </div>
     <?php endforeach; ?>
   </div>
 
---
+++ layout/header.php
@@ -0,0 +1,5 @@
+<header>
+  <div class="container p-2 text-center">
+    <h2>Animalstore</h2>
+  </div>
+</header>
